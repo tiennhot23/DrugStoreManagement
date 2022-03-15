8ff0bf5f776a24d9256b239194415d312440860a
Đoàn Việt Hưng <doanviethung99@gmail.com>
2022-03-15 12:47:32 +0700

    tét

---
+++ app/src/main/java/com/example/drugstoremanagement/ui/bill/BillActivity.java
@@ -0,0 +1,16 @@
+package com.example.drugstoremanagement.ui.bill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drugstoremanagement.R;
+
+public class BillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
