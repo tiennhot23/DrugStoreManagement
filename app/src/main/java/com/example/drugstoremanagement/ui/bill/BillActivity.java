5247c6e3caac1f6ad00e74a50bdbefeebdc8dcd1
Đoàn Việt Hưng <doanviethung99@gmail.com>
2022-03-15 13:01:40 +0700

    tao cai me gi do

--- app/src/main/java/com/example/drugstoremanagement/ui/bill/BillActivity.java
+++ app/src/main/java/com/example/drugstoremanagement/ui/bill/BillActivity.java
@@ -3,14 +3,18 @@ package com.example.drugstoremanagement.ui.bill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.drugstoremanagement.R;
 
 public class BillActivity extends AppCompatActivity {
 
+    private TextView áaaa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill);
+
     }
 }
