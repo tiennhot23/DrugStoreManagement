62c804f520aea6c9d9eb6ea95b0fd6b20022a126
tiennhot23 <tiennhot23@gmail.com>
2022-04-19 11:42:59 +0700

    custom switch, chart

--- app/src/main/java/com/example/drugstoremanagement/ui/statistic/StatisticFragment.java
+++ app/src/main/java/com/example/drugstoremanagement/ui/statistic/StatisticFragment.java
@@ -2,6 +2,7 @@ package com.example.drugstoremanagement.ui.statistic;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,15 @@ import com.example.drugstoremanagement.data.db.model.DrugStore;
 import com.example.drugstoremanagement.data.viewmodel.DrugStoreViewModel;
 import com.example.drugstoremanagement.ui.base.BaseFragment;
 import com.example.drugstoremanagement.ui.detailbill.DetailBillActivity;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -26,6 +36,8 @@ public class StatisticFragment extends BaseFragment {
     private ExpandableListView expandableListView;
     private ExpandableListBillAdapter adapter;
     private TextView txtTotal;
+    private Switch btnSwitch;
+    private BarChart chart;
 
     private DrugStoreNameAdapter drugStoreAdapter;
     List<String> dates;
@@ -63,12 +75,23 @@ public class StatisticFragment extends BaseFragment {
             startActivity(intent);
             return false;
         });
+        btnSwitch.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                expandableListView.setVisibility(View.GONE);
+                chart.setVisibility(View.VISIBLE);
+            } else {
+                expandableListView.setVisibility(View.VISIBLE);
+                chart.setVisibility(View.GONE);
+            }
+        });
     }
 
     private void setupView(View view) {
         spinnerDrugStore = view.findViewById(R.id.spinner);
         expandableListView = view.findViewById(R.id.list_item);
         txtTotal = view.findViewById(R.id.txt_total);
+        btnSwitch = view.findViewById(R.id.btn_switch);
+        chart = view.findViewById(R.id.chart);
     }
 
     private void setupSpinner() {
@@ -80,6 +103,7 @@ public class StatisticFragment extends BaseFragment {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 loadData(drugStoreAdapter.getItemID(i));
+                loadChart();
                 adapter.notifyDataSetChanged();
             }
 
@@ -115,5 +139,40 @@ public class StatisticFragment extends BaseFragment {
         }
         dates.addAll(diffDates);
         txtTotal.setText(String.valueOf(total));
+    }
+
+    private void loadChart() {
+        ArrayList<BarEntry> income = new ArrayList<>();
+        for (int i=0; i<dates.size(); i++) {
+            int total = 0;
+            for (Bill bill : Objects.requireNonNull(data.get(dates.get(i)))) {
+                total += bill.getTotal();
+            }
+            income.add(new BarEntry(i, total));
+        }
+        chart.getXAxis().setXOffset(1f);
+        chart.getXAxis().setAxisMaximum(dates.size());
+
+        chart.getXAxis().setGranularity(1f);
+        chart.getXAxis().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                int val = (int) value;
+                if (val == 0) {
+                    return dates.get(val);
+                } else return "";
+            }
+        });
+
+
+        BarDataSet barDataSet = new BarDataSet(income, "Doanh thu");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        BarData barData = new BarData(barDataSet);
+        chart.setFitBars(true);
+        chart.setData(barData);
+        chart.animateY(2000);
+
     }
 }
